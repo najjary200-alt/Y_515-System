e62b7ba5d308bce6efdab3b57addee5b50cd9d3d
najjary200-alt <najjary200@gmail.com>
2026-08-28 11:43:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.login("MTU0Mjc1NjAzOTU1NDc2ODk1Ng.G_HNWJ.S9d21Z8UTPP42HjAtz1_71BmmChiWL9705GDls");
+client.login(process.env.TOKEN);
+
 
 
 
