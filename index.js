89fc0fa98febc4a654e74fac81acfd5a19555d55
najjary200-alt <najjary200@gmail.com>
2026-08-28 11:55:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
@@ -13,7 +12,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.login("MTU0Mjc1NjAzOTU1NDc2ODk1Ng.Gk_UwM.Rye88kc7pLbkl6B0ibSjrqPLAltSxP09hH3At4");
+client.login("MTU0Mjc1NjAzOTU1NDc2ODk1Ng.GIAuv1.ITuT0UCFdH7jjlTzQMK9WK86BzT0TPAONOwfSk");
 
 
 
